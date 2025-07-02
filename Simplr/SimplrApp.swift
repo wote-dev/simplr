02fb3ba808da432a846c5d0ccf6af83fb3ee66d7
whispersoftheelders <205708280+wote-dev@users.noreply.github.com>
2025-07-02 13:20:54 +1000

    Initial Commit

---
+++ Simplr/SimplrApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimplrApp.swift
+//  Simplr
+//
+//  Created by Daniel Zverev on 2/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct SimplrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SimplrTests/SimplrTests.swift
@@ -0,0 +1,17 @@
+//
+//  SimplrTests.swift
+//  SimplrTests
+//
+//  Created by Daniel Zverev on 2/7/2025.
+//
+
+import Testing
+@testable import Simplr
+
+struct SimplrTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
